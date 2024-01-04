2779f0d5be4baa01185636c5c505fdd86fff90e8
Simona Pernišová <smnprnsv@gmail.com>
2024-01-04 13:48:31 +0100

    Okomentovanie servera a klienta pre lepšiu orientáciu

--- server.c
+++ server.c
@@ -1,4 +1,3 @@
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <stdio.h>
@@ -13,42 +12,57 @@ int main(int argc, char *argv[])
     struct sockaddr_in serv_addr, cli_addr;
     int n;
     char buffer[256];
+    //skontrolujeme pocet argumentov ,ak je mensi ako 2 tak sme nezadali port
+
     if (argc < 2)
     {
-       fprintf(stderr,"usage %s port\n", argv[0]);
+        fprintf(stderr,"usage %s port\n", argv[0]);
         return 1;
     }
-    printf("tu som 1 \n");
+
+    //vycistenie struktury ktoru nastavujeme (typ socketov , rozsah ip adries a port)
     bzero((char*)&serv_addr, sizeof(serv_addr));
+    //informacie o tom aký typ socketov chcem použiť (AF_INET -internetove sockety)
     serv_addr.sin_family = AF_INET;
+    //odkial budem chcet pocuvat /primat spojenie -rozzsah ip adries alebo maska podsiete (mame nastaveny cely internet)
     serv_addr.sin_addr.s_addr = INADDR_ANY;
+    //informacia na akom porte pocuvam -htons (potrebujeme big endian a prevadzame little endian to big endian)
     serv_addr.sin_port = htons(atoi(argv[1]));
 
+    //jednoznacne identifikovanie socketu
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    //ak unixove volanie zlyha tak vrati hodnotu mensiu ako 0
     if (sockfd < 0)
     {
         perror("Error creating socket");
         return 1;
     }
-    printf("tu som 2 \n");
+
+    //vytvorenie danej struktury
     if (bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
     {
         perror("Error binding socket address");
         return 2;
     }
 
-    listen(sockfd, 5);
+    //pasivny socket - nie je urceny na komunikaciu s klietom ale na pripojenie
+    //potom si server vytvori dalsi socket ktorym uz bude komunikovat s klientom
+    listen(sockfd, 5); //v jednom okamziku sa mi moze pripojitmaximalne 5 klientov
     cli_len = sizeof(cli_addr);
 
+    //tu sa caka kym sa niekto pripoji
     newsockfd = accept(sockfd, (struct sockaddr*)&cli_addr, &cli_len);
     if (newsockfd < 0)
     {
         perror("ERROR on accept");
         return 3;
     }
-    printf("tu som 3 \n");
+    //vynulovany buffer
     bzero(buffer,256);
+    //precitam a ulozim informcie do buffra - max 255 bajtov
     n = read(newsockfd, buffer, 255);
+    //bajtov nesmie byt menej ako 1
     if (n < 0)
     {
         perror("Error reading from socket");
@@ -57,13 +71,14 @@ int main(int argc, char *argv[])
     printf("Here is the message: %s\n", buffer);
 
     const char* msg = "I got your message";
+    //mag + 1 ,pretoze je tu este /0 na ukoncenie
     n = write(newsockfd, msg, strlen(msg)+1);
     if (n < 0)
     {
         perror("Error writing to socket");
         return 5;
     }
-
+    //ukoncenie komunikacie
     close(newsockfd);
     close(sockfd);
 
